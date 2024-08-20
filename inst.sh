900b29ae24f36c8d1d98b55e8eebdfd007bb2d0c
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-20 23:04:15 +0700

    inst.sh

---
+++ inst.sh
@@ -0,0 +1,1 @@
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
