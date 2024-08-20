5ec3d46c7d05d225f3fcdc5289db0cfea37e3a95
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-20 23:51:50 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -4,4 +4,4 @@ clear
 echo "Đang cài đặt vui lòng chờ"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/v3.zip && unzip v3.zip && chmod a+x nek && chmod +x * && mv mod login && login
