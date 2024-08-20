65accc948ed4422139451b2e29ca83477e4ef58b
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-20 22:00:09 +0700

    Add files via upload

---
+++ inst.sh
@@ -0,0 +1,15 @@
+clear
+termux-setup-storage
+clear
+echo "Đang cài đặt"
+apt update && apt upgrade -y
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/tun.zip && unzip tun.zip && chmod a+x nek && chmod a+x 4g && chmod a+x psiphon-tunnel-core && chmod a+x qex && chmod a+x wl && chmod a+x mod
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/t1 && mv t1 motd
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/l1 && chmod +x * && mv l1 login &&
+clear
+echo "Đã cài đặt thành công"
+login
+chmod +x * && mv mod login && login
