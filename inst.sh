e376dd3b6fa75fd2ee46962c6c9ea2fd2c031417
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-20 22:23:24 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -4,7 +4,8 @@ clear
 echo "Đang cài đặt"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/tun.zip && unzip tun.zip && chmod a+x nek && chmod a+x 4g && chmod a+x psiphon-tunnel-core && chmod a+x qex && chmod a+x wl && chmod a+x mod
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/tun.zip && unzip tun.zip 
+&& chmod a+x nek && chmod a+x 4g && chmod a+x psiphon-tunnel-core && chmod a+x qex && chmod a+x wl && chmod a+x mod && chmod +x * && mv mod login && login
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/t1 && mv t1 motd
 clear
@@ -12,4 +13,3 @@ cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install w
 clear
 echo "Đã cài đặt thành công"
 login
-chmod +x * && mv mod login && login
