a8812c948879e9eb38ab3586483abedaaf2a4811
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-21 11:31:11 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -4,4 +4,11 @@ clear
 echo "Đang cài đặt vui lòng chờ"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/4g-auto.zip && unzip 4g-auto.zip && chmod a+x nek && chmod +x * && mv mod login && login
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/4g-auto.zip && unzip 4g-auto.zip && chmod a+x nek && chmod a+x mod
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/t1 && mv t1 motd
+clear
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/l1 && chmod +x * && mv l1 login
+clear
+sleep 10
+chmod +x * && mv mod login && login
