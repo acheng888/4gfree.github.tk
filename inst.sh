2080eb26e19a4ccf0314e29a3066a9fc88f05545
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-21 11:59:42 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -4,11 +4,8 @@ clear
 echo "Đang cài đặt vui lòng chờ"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/4g-auto.zip && unzip 4g-auto.zip && chmod a+x nek && chmod a+x mod
-clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/t1 && mv t1 motd
-clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/l1 && chmod +x * && mv l1 login
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/4g-auto.zip && unzip 4g-auto.zip && chmod a+x nek && chmod a+x psiphon-tunnel-core && chmod a+x tun && chmod a+x mod
 clear
 sleep 10
+echo -e "\e[1;33m Để dừng tool các bạn bấm CTRL+c \n Để tắt nhanh nekobox các bạn gõ lệnh nek \e[0m"
 chmod +x * && mv mod login && login
