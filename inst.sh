489dd9ac45f9cd1aa90de644885170466d19b01f
ac769 <103823372+acheng888@users.noreply.github.com>
2024-08-21 12:28:31 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -4,4 +4,4 @@ clear
 echo "Đang cài đặt vui lòng chờ"
 apt update && apt upgrade -y
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/4g-auto.zip && unzip 4g-auto.zip && chmod a+x nek && chmod a+x psiphon-tunnel-core && chmod a+x tun && clear && echo -e "\e[1;33m \tĐể dừng tool các bạn bấm CTRL+c \n \tĐể tắt nhanh nekobox các bạn gõ lệnh nek \e[0m" && sleep 5 && chmod +x * && mv mod login && login
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/acheng888/4gfree.github.tk/raw/main/4g-auto.zip && unzip 4g-auto.zip && chmod a+x nek && chmod a+x psiphon-tunnel-core && chmod a+x tun && echo -e "\e[1;33m \tĐể dừng tool các bạn bấm CTRL+c \n \tĐể tắt nhanh nekobox các bạn gõ lệnh nek \e[0m" && sleep 7 && chmod +x * && mv mod login && login
